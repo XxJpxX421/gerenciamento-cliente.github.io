043b8450ed4900f0ada3170adb17fb996d7817d7
XxJpxX421 <joaopedrobordasilva@gmail.com>
2023-05-29 14:58:22 -0300

    trabalho

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Keania+One&display=swap" rel="stylesheet" />
+<link href="./css/login.css" rel="stylesheet" />
+<title>Login</title>
+</head>
+<body>
+    <div class="fundo">
+        <div class="container"></div>
+        <span class="login">Login</span>
+        <div class="ret-email"></div>
+        <span class="email">Email:</span>
+        <span class="senha">Senha:</span>
+        <div class="ret-senha"></div>
+        <input type="password" name="senha" id="senha">
+        <div class="ret-botoes"></div>
+        <div class="ret-cadastrar"></div>
+        <div class="ret-logar"></div>
+        <div class="ret-ajuda"></div>
+        <input type="email" name="login" id="login">
+        <div class="name"></div>
+        <span class="ajuda">Ajuda</span>
+        <div class="ret-dados"></div>
+        <a href="senha.php"><span class="dados">Banco de Dados</span></a>
+        <span class="logar">Logar</span>
+        <span class="cadastrar">Cadastre-se</span>
+        <div class="logo"></div>
+    </div>
+</body>
+</html>
---
+++ senha.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Keania+One&display=swap" rel="stylesheet" />
+        <link href="./css/senha.css" rel="stylesheet" />
+        <title>Acessar Banco de Dados</title>
+    </head>
+    <body>
+        <div class="fundo">
+            <div class="logo"></div>
+            <div class="container"></div>
+            <div class="ret-botoes">
+            </div>
+            <div class="botao-enviar"></div>
+            <div class="botao-voltar"></div>
+            <span class="enviar">Entrar</span>
+            <a href="login.php"><span class="voltar">Voltar</span></a>
+            <span class="titulo">Esta Área é apenas para Administradores!! Por favor, para acessá-la, insira a senha abaixo.</span>
+            <div class="ret-senha">
+            </div>
+            <input type="password" name="senha" id="senha">
+        </div>
+    </body>
+    </html>
